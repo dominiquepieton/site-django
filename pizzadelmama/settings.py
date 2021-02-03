94562509d5c29f13f428221a577821c931670b86
dominiquepieton <pietondominique@yahoo.fr>
2021-02-03 12:38:38 +0100

    modify settig

--- pizzadelmama/settings.py
+++ pizzadelmama/settings.py
@@ -111,7 +111,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
@@ -119,8 +119,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+# )
 # Configure Django App for Heroku
 django_on_heroku.settings(locals())
